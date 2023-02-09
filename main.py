68b0880ff5202b017db2f20a2a013da4de084781
Tomi (tk0) Koski <1329786+tomikoski@users.noreply.github.com>
2023-02-09 11:31:21 +0200

    support for random user-agent and multiple downloads

--- main.py
+++ main.py
@@ -84,124 +84,125 @@ def main():
     # take the package_id from the user
     package_id = sys.argv[1]
 
-    print('%sGetting download link... %s' %
-          (fg('light_yellow'), attr('reset')))
+    for onepackage in package_id.split(','):
 
-    # verify g_play_url with packageId string
-    g_play_res = requests.get(g_play_url + package_id,
-                              headers=headers, allow_redirects=True)
-    if(g_play_res.status_code != 200):
-        show_invalid_id_err()
+       package_id = onepackage
 
-    # search the web page using the package id
-    search_res = requests.get(search_url + package_id,
-                              headers=headers, allow_redirects=True)
-
-    # check the statuscode and verify it
-    if search_res.status_code != 200:
-        show_internet_error()
-
-    # there will be a list of apps that show on the website
-    soup = BeautifulSoup(search_res.content, 'html.parser')
-
-    software_names = [SoftwareName.CHROME.value, SoftwareName.FIREFOX.value, SoftwareName.EDGE.value, SoftwareName.OPERA.value]
-    operating_systems = [OperatingSystem.WINDOWS.value, OperatingSystem.LINUX.value]   
-    user_agent_rotator = UserAgent(software_names=software_names, operating_systems=operating_systems, limit=100)
-    # Get list of user agents.
-    user_agents = user_agent_rotator.get_user_agents()
-    # Get Random User Agent String.
-    user_agent = user_agent_rotator.get_random_user_agent()
-
-    headers['User-Agent'] = user_agent
-    print("Today's lucky winnder for 'User-Agent' is: %s%s%s ..." %
-          (fg('light_blue'), user_agent, attr('reset')))
-
-    # # find the first class with the class name "search-dl"
-    # children = soup.findChildren('a', class_='col col-6 list')
-
-    # # get href of the first child
-    # first_child = children[0]
-    # app_href = first_child.get('href')
-
-    # # get the name of the child
-    # app_name = first_child.find('strong').text
-
-    # # hyphenate and format the app name into lowercase
-    # app_name_formatted = app_name.replace(" ", "-").lower()
-    # app_url = version_url + app_href
-
-    # # get string after the last '/'
-    # app_id = app_href.rsplit('/', 1)[1]
-
-    # app_response = requests.get(version_url + app_id,
-    #                             headers=headers, allow_redirects=True)
-
-    # if app_response.status_code != 200:
-    #     show_internet_error()
-
-    tbody_children = soup.findAll('a')
-
-    sub_dl_links = []
-
-    # tbody_children
-    # loop through the children and get the href
-    for item in tbody_children:
-        # get href of the child
-        link = item.get('href')
-        # if link contains "downwload"
-        if link.find("/download/") != -1:
-            # append the link to the list
-            sub_dl_links.append(base_url+link)
-
-    # establish a connection to the first link
-    sub_dl_res = requests.get(sub_dl_links[0],
-                              headers=headers, allow_redirects=True)
-    if sub_dl_res.status_code != 200:
-        show_internet_error()
-
-    # locate the script, get the contents
-    script_text = BeautifulSoup(
-        sub_dl_res.content, 'html.parser').findAll("script")
-
-    # find the last but one script tag
-    last_script = script_text[-2].contents[0]
-
-    # query the token from the script
-    token = re.findall("token','([^\']+)", last_script)[0]
-
-    # make a request to the download link
-    dl_res = requests.post(dl_url, data={'token': token}, headers=headers)
-    if dl_res.status_code != 200:
-        show_internet_error()
-    dl_res_json = dl_res.json()
-    # get the download_link from the json response
-    download_link = dl_res_json['download_link']
-
-    # download the apk
-    print('%sDownloading APK 🚀🚀🚀 %s' %
-          (fg('yellow'), attr('reset')))
-
-    # TODO: replace app_name with actual app name
-    output_file = "output/" + "app_name" + ".apk"
-
-    r = requests.get(download_link, allow_redirects=True, stream=True)
-    with open(output_file, 'wb') as f:
-        total_length = int(r.headers.get('content-length'))
-        bar = make_progress_bar()
-        bar.start(total_length)
-        dl = 0
-        for chunk in r.iter_content(chunk_size=1024):
-            if chunk:
-                dl += len(chunk)
-                f.write(chunk)
-                bar.update(dl)
-        bar.finish()
-    print('%sAPK Downloaded %s' %
-          (fg('green'), attr('reset')))
-
-    print('%sAPK DOWNLOADED : App saved to output/app.apk %s' %
-          (fg('green'), attr('reset')))
-
+       print('%sGetting download link... %s' %
+             (fg('light_yellow'), attr('reset')))
+   
+       # verify g_play_url with packageId string
+       g_play_res = requests.get(g_play_url + package_id,
+                                 headers=headers, allow_redirects=True)
+       if(g_play_res.status_code != 200):
+           show_invalid_id_err()
+   
+       # search the web page using the package id
+       search_res = requests.get(search_url + package_id,
+                                 headers=headers, allow_redirects=True)
+   
+       # check the statuscode and verify it
+       if search_res.status_code != 200:
+           show_internet_error()
+   
+       # there will be a list of apps that show on the website
+       soup = BeautifulSoup(search_res.content, 'html.parser')
+   
+       software_names = [SoftwareName.CHROME.value, SoftwareName.FIREFOX.value, SoftwareName.EDGE.value, SoftwareName.OPERA.value]
+       operating_systems = [OperatingSystem.WINDOWS.value, OperatingSystem.LINUX.value, OperatingSystem.MAC.value]
+       user_agent_rotator = UserAgent(software_names=software_names, operating_systems=operating_systems, limit=100)
+       user_agent = user_agent_rotator.get_random_user_agent()
+   
+       headers['User-Agent'] = user_agent
+       print("Today's lucky winnder for 'User-Agent' is: %s%s%s ..." %
+             (fg('light_blue'), user_agent, attr('reset')))
+   
+       # # find the first class with the class name "search-dl"
+       # children = soup.findChildren('a', class_='col col-6 list')
+   
+       # # get href of the first child
+       # first_child = children[0]
+       # app_href = first_child.get('href')
+   
+       # # get the name of the child
+       # app_name = first_child.find('strong').text
+   
+       # # hyphenate and format the app name into lowercase
+       # app_name_formatted = app_name.replace(" ", "-").lower()
+       # app_url = version_url + app_href
+   
+       # # get string after the last '/'
+       # app_id = app_href.rsplit('/', 1)[1]
+   
+       # app_response = requests.get(version_url + app_id,
+       #                             headers=headers, allow_redirects=True)
+   
+       # if app_response.status_code != 200:
+       #     show_internet_error()
+   
+       tbody_children = soup.findAll('a')
+   
+       sub_dl_links = []
+   
+       # tbody_children
+       # loop through the children and get the href
+       for item in tbody_children:
+           # get href of the child
+           link = item.get('href')
+           # if link contains "downwload"
+           if link.find("/download/") != -1:
+               # append the link to the list
+               sub_dl_links.append(base_url+link)
+   
+       # establish a connection to the first link
+       sub_dl_res = requests.get(sub_dl_links[0],
+                                 headers=headers, allow_redirects=True)
+       if sub_dl_res.status_code != 200:
+           show_internet_error()
+   
+       # locate the script, get the contents
+       script_text = BeautifulSoup(
+           sub_dl_res.content, 'html.parser').findAll("script")
+   
+       # find the last but one script tag
+       last_script = script_text[-2].contents[0]
+   
+       # query the token from the script
+       token = re.findall("token','([^\']+)", last_script)[0]
+   
+       # make a request to the download link
+       dl_res = requests.post(dl_url, data={'token': token}, headers=headers)
+       if dl_res.status_code != 200:
+           show_internet_error()
+       dl_res_json = dl_res.json()
+       # get the download_link from the json response
+       download_link = dl_res_json['download_link']
+   
+       # download the apk
+       print('%sDownloading APK 🚀🚀🚀 %s' %
+             (fg('yellow'), attr('reset')))
+   
+       # TODO: replace app_name with actual app name
+       output_file = "output/" + package_id + ".apk"
+   
+       r = requests.get(download_link, allow_redirects=True, stream=True)
+       with open(output_file, 'wb') as f:
+           total_length = int(r.headers.get('content-length'))
+           bar = make_progress_bar()
+           bar.start(total_length)
+           dl = 0
+           for chunk in r.iter_content(chunk_size=1024):
+               if chunk:
+                   dl += len(chunk)
+                   f.write(chunk)
+                   bar.update(dl)
+           bar.finish()
+       print('%sAPK Downloaded %s' %
+             (fg('green'), attr('reset')))
+   
+       print('%sAPK DOWNLOADED : App saved to output/%s.apk %s' %
+             (fg('green'), package_id, attr('reset')))
+   
     exit()
 
 
